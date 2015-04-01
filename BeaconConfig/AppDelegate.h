729797b7de2d2331f2295c2ec803f116550a8696
Baoming Bai <tskyming@163.com>
2015-04-01 11:28:29 +0800

    Initial Commit

---
+++ BeaconConfig/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeaconConfig
+//
+//  Created by skyming on 15/4/1.
+//  Copyright (c) 2015年 Sensoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeaconConfig/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaconConfig
+//
+//  Created by skyming on 15/4/1.
+//  Copyright (c) 2015年 Sensoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
